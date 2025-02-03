bf37bcf4d6cde92c5abfd04080c1bc8eebc697a0
sujatagunale <sujata@jsmastery.pro>
2025-02-04 00:41:56 +0530

    feat: error middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 import connectDB from "./database/mongodb.js";
 
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -11,10 +13,16 @@ import subscriptionRouter from "./routes/subscription.route.js";
 
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(arcjetMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
